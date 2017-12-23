343c590f8e0caa616ffa66500c6ba0a292b7e822
Huong <you@example.com>
2017-12-23 19:06:38 +0700

    tiki

--- baitap2.1.php
+++ baitap2.1.php
@@ -1,7 +1,43 @@
 <?php
 
 $content = file_get_contents('https://tiki.vn/tai-nghe-nhac/c1804');
-echo $content;
 
+$partern = '/class="product-box-list".*>(.*).*class="list-pager"/imsxU';
+preg_match_all($partern,$content,$matches);
+// echo "<pre>";
+// print_r($matches);
+// echo "</pre>";
+
+//name
+preg_match_all('/class="title">(.*)<\/span>/imsxU',$matches[0][0],$name);
+
+echo "<pre>";
+print_r($name[1]);
+echo "</pre>";
+
+//image
+preg_match_all('/class="product-image.*src="(.*)".*>/misU',$matches[0][0],$images);
+
+
+echo "<pre>";
+print_r($images[1]);
+echo "</pre>";
+
+//$sql = "INSERT INTO products(name,image) VALUES ('$name','$image')"
+
+foreach($name[1] as $key=>$tensp){
+    echo $tensp;
+    echo "<br>";
+    $img = $images[1][$key];
+    echo "<img src='$img'>";
+    echo "<hr>";
+
+}
 
 ?>
+
+<!-- <span class="title">(.*)</span> -->
+
+<!-- class="product-box-list".*>(.*).*class="list-pager" -->
+
+<!-- class="product-image img-responsive lazy".*src="(.*)".*> -->
--- buoi2.php
+++ buoi2.php
@@ -10,41 +10,41 @@
 // print_r($matches);
 // echo "</pre>";
 
-// $str = '<form>
-//   <div class="form-group">
-//     <label for="exampleInputEmail1">Email address</label>
-//     <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-//     <small id="emailHelp" class="form-text text-muted">Well never share your email with anyone else.</small>
-//   </div>
-//   <div class="form-group">
-//     <label for="exampleInputPassword1">Password 1</label>
-//     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-//   </div>
-//   <div class="form-group">
-//     <label for="exampleInputPassword1">Password 2</label>
-//     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-//   </div>
-//   <div class="form-group">
-//     <label for="exampleInputPassword1">Password 3</label>
-//     <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-//   </div>
-//   <div class="form-check">
-//     <label class="form-check-label">
-//       <input type="checkbox" class="form-check-input">
-//       Check me out
-//     </label>
-//   </div>
-//   <button type="submit" class="btn btn-primary">Submit</button>
-// </form>';
+$str = '<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">Well never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password 1</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password 2</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password 3</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-check">
+    <label class="form-check-label">
+      <input type="checkbox" class="form-check-input">
+      Check me out
+    </label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>';
 
-// $partern ='/for="exampleInputPassword1">(\w+)*\s(\d)*<\/label>/';
+$partern ='/for="exampleInputPassword1">(\w+)*\s(\d)*<\/label>/';
 
-// preg_match_all($partern,$str,$matches);
+preg_match_all($partern,$str,$matches);
 
-// echo "<pre>";
-// print_r($matches);
-// echo "</pre>";
-
+echo "<pre>";
+print_r($matches);
+echo "</pre>";
+die;
 
 // $str = "Luôn luôn lắng nghe";
 
