aae76583c4337a4dca635acfc8808f967e13417c
Huong <you@example.com>
2018-01-11 17:39:03 +0700

    PDO

---
+++ buoi9/PDO.php
@@ -0,0 +1,7 @@
+<?php
+//https://github.com/huongnguyen08/php2/blob/master/B%C3%A0i%2017%20-%20K%E1%BA%BFt%20h%E1%BB%A3p%20PDO%20v%C3%A0%20MySQL.pdf
+
+//https://www.w3resource.com/mysql/mysql-tutorials.php
+
+
+?>
